c84e39ab9eb35fd005a18fa3fc0b3a771afbfd44
Daniel Bigos <daniel.bigos@icloud.com>
2025-08-26 16:54:28 +0200

    feat: implement Beacon Proxy contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,45 @@
-//!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```solidity
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-//!
-// Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
-#![cfg_attr(not(any(test, feature = "export-abi")), no_std)]
-
-#[macro_use]
 extern crate alloc;
 
 use alloc::vec::Vec;
 
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
+use openzeppelin_stylus::proxy::{beacon::proxy::BeaconProxy, erc1967, IProxy};
+use stylus_sdk::{abi::Bytes, alloy_primitives::Address, prelude::*, ArbResult};
 
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct Counter {
-        uint256 number;
+#[entrypoint]
+#[storage]
+struct BeaconProxyWorkshop {
+    beacon_proxy: BeaconProxy,
+}
+
+#[public]
+impl BeaconProxyWorkshop {
+    #[constructor]
+    pub fn constructor(
+        &mut self,
+        beacon: Address,
+        data: Bytes,
+    ) -> Result<(), erc1967::utils::Error> {
+        self.beacon_proxy.constructor(beacon, &data)
+    }
+
+    fn implementation(&self) -> Result<Address, Vec<u8>> {
+        IProxy::implementation(self)
+    }
+
+    fn get_beacon(&self) -> Address {
+        self.beacon_proxy.get_beacon()
+    }
+
+    #[fallback]
+    fn fallback(&mut self, calldata: &[u8]) -> ArbResult {
+        unsafe { self.do_fallback(calldata) }
     }
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
-#[public]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
-
-    /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
-    }
-
-    /// Adds the wei value from msg_value to the number in storage.
-    #[payable]
-    pub fn add_from_msg_value(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + self.vm().msg_value());
+unsafe impl IProxy for BeaconProxyWorkshop {
+    fn implementation(&self) -> Result<Address, Vec<u8>> {
+        self.beacon_proxy.implementation()
     }
 }
 
